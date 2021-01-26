86c7e54982c2e7a92014a2d17e4c6ccede1b6343
Omkar Kulkarni <omkarak45@gmail.com>
2021-01-26 12:32:54 +0530

    :hammer: [Fix] CORS bug fixed

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
+const helmet = require("helmet");
 const connectDB = require("./config/db");
 const bodyParser = require("body-parser");
 const errorHandler = require("./middleware/error.middleware");
+const cors = require("cors");
+const corsOptions = require("./utils/corsOptions");
 const app = express();
 connectDB();
 
 const PORT = process.env.PORT || 5000;
-
+app.use(helmet());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/dashboard", require("./routes/dashboard.routes"));
