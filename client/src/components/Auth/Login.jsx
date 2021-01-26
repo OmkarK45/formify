7fcac7185310967b8a7247b8d951f0d6e2415974
Omkar Kulkarni <omkarak45@gmail.com>
2021-01-26 12:29:23 +0530

    :lock: Added Auth Post
    @TODO : Write redirects

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { POST } from "../../utils/network";
 import {
   Input,
   FormControl,
@@ -5,40 +7,58 @@ import {
   Box,
   Heading,
   Button,
-  FormHelperText,
 } from "@chakra-ui/react";
-
+// @TODO - Importing react query, sending data to server, storing token in cookie or LS and settings context
 const Login = () => {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+    isSubmitting: false,
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    return POST(process.env.REACT_APP_BACKEND + "/api/auth/login", {
+      email: data.email,
+      password: data.password,
+    })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
+  const handleInputChange = (event) => {
+    setData({
+      ...data,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <>
       <Box>
         <Heading>Login.</Heading>
       </Box>
-      <form>
+      <form onSubmit={handleSubmit}>
         <FormControl marginTop="1.5rem">
           <FormLabel htmlFor="email">Email address</FormLabel>
           <Input
             type="email"
-            id="email"
             name="email"
-            //   value={email}
-            //   onChange={handleEmail}
+            value={data.email}
+            onChange={handleInputChange}
             aria-describedby="email-helper-text"
+            id="email"
           />
-          <FormHelperText id="email-helper-text">
-            We'll never share your email.
-          </FormHelperText>
         </FormControl>
 
         <FormControl marginTop="2rem">
           <FormLabel htmlFor="password">Password</FormLabel>
           <Input
             type="password"
-            id="password"
             name="password"
-            //   value={password}
-            //   onChange={handlePassword}
-            aria-describedby="email-helper-text"
+            value={data.password}
+            onChange={handleInputChange}
+            id="password"
           />
         </FormControl>
         <Button
@@ -48,6 +68,7 @@ const Login = () => {
           color="white"
           marginBottom="1rem"
           colorScheme="teal"
+          type="submit"
         >
           Login
         </Button>
