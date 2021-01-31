b7a2f088bb0c8e4e74f39c6516b8d6ab1f539d00
Omkar Kulkarni <omkarak45@gmail.com>
2021-01-31 20:11:25 +0530

    Misc Options

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
 const connectDB = require("./config/db");
 const bodyParser = require("body-parser");
@@ -10,11 +11,15 @@ const app = express();
 connectDB();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(cookieParser());
 app.use(helmet());
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/dashboard", require("./routes/dashboard.routes"));
+app.use("/api/forms/", require("./routes/form.routes"));
 app.use(errorHandler);
 
 const server = app.listen(PORT, () => {
