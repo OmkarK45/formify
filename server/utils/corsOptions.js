ee89ce7c48cf5e21f1af1c4d85856db513bd11e5
Omkar Kulkarni <omkarak45@gmail.com>
2021-01-31 20:18:22 +0530

    Revert "Misc Options"
    This reverts commit b7a2f088bb0c8e4e74f39c6516b8d6ab1f539d00.

--- server/utils/corsOptions.js
+++ server/utils/corsOptions.js
@@ -1,7 +1,5 @@
-//  @TODO -[Security] Change this to frontend later
 const corsOptions = {
   origin: "http://localhost:3000",
   optionsSuccessStatus: 200,
-  credentials: true,
 };
 module.exports = corsOptions;
