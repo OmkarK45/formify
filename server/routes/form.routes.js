1b7b91bb74323a9750617fde953d4bcf5038e4f8
Omkar Kulkarni <omkarak45@gmail.com>
2021-02-01 15:27:14 +0530

    Added routes

--- server/routes/form.routes.js
+++ server/routes/form.routes.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const { getForm, createForm } = require("../controllers/form.controller");
+const {
+  getOneForm,
+  getForms,
+  createForm,
+  postOneForm,
+} = require("../controllers/form.controller");
 const { protect } = require("../middleware/auth.middleware");
 // @routes -> you are here /api/forms->
-router.route("/:username/").get(protect, getForm);
+router.route("/:username/").get(protect, getForms);
+
+// @desc -> get one form and see its submissions
+router.route("/:username/:formID").get(protect, getOneForm);
+
+// @desc -> get one form with given ID and submit to the fields array
+router.route("/:username/:formID").post(protect, postOneForm);
 // @todo -> add form middleware here
-router.route("/:username/:formId").post(protect, createForm);
+router.route("/:email/").post(protect, createForm);
+
 module.exports = router;
