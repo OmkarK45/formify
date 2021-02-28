a05f02e6c531b23113c52bbea21e2f3bf271b438
Omkar Kulkarni <omkarak45@gmail.com>
2021-02-28 21:01:55 +0530

    Added email template and improved static page

---
+++ server/utils/newSubmissionTemplate.js
@@ -0,0 +1,197 @@
+exports.newSubmissionTemplate = (formName) => {
+  return `
+    <!DOCTYPE html>
+    <html>
+    <head>
+    
+      <meta charset="utf-8">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <title>New Submission!</title>
+      <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <style type="text/css">
+      body,
+      table,
+      td,
+      a {
+        font-family:"Inter",system-ui;
+        -ms-text-size-adjust: 100%; /* 1 */
+        -webkit-text-size-adjust: 100%; /* 2 */
+      }
+    
+      table,
+      td {
+        mso-table-rspace: 0pt;
+        mso-table-lspace: 0pt;
+      }
+    
+      img {
+        -ms-interpolation-mode: bicubic;
+      }
+    
+      a[x-apple-data-detectors] {
+        font-family: inherit !important;
+        font-size: inherit !important;
+        font-weight: inherit !important;
+        line-height: inherit !important;
+        color: inherit !important;
+        text-decoration: none !important;
+      }
+    
+      div[style*="margin: 16px 0;"] {
+        margin: 0 !important;
+      }
+      body {
+        width: 100% !important;
+        height: 100% !important;
+        padding: 0 !important;
+        margin: 0 !important;
+      }
+    
+      table {
+        border-collapse: collapse !important;
+      }
+      a {
+        color: #1a82e2;
+      }
+      img {
+        height: auto;
+        line-height: 100%;
+        text-decoration: none;
+        border: 0;
+        outline: none;
+      }
+      </style>
+    
+    </head>
+    <body style="background-color: #e9ecef;">
+    
+    
+      <div class="preheader" style="display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;">
+        Your form has received a new submission!
+      </div>
+    
+      <table border="0" cellpadding="0" cellspacing="0" width="100%">
+    
+        <tr>
+          <td align="center" bgcolor="#e9ecef">
+            <!--[if (gte mso 9)|(IE)]>
+            <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+            <tr>
+            <td align="center" valign="top" width="600">
+            <![endif]-->
+            <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+              <tr>
+                <td align="center" valign="top" style="padding: 36px 24px;">
+                  <a href="https://sendgrid.com" target="_blank" style="display: inline-block;">
+                    <img src="https://tailwindui.com/img/logos/workflow-mark-orange-500.svg" alt="Logo" border="0" width="48" style="display: block; width: 48px; max-width: 48px; min-width: 48px;">
+                  </a>
+                </td>
+              </tr>
+            </table>
+            <!--[if (gte mso 9)|(IE)]>
+            </td>
+            </tr>
+            </table>
+            <![endif]-->
+          </td>
+        </tr>
+    
+        <tr>
+          <td align="center" bgcolor="#e9ecef">
+            <!--[if (gte mso 9)|(IE)]>
+            <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+            <tr>
+            <td align="center" valign="top" width="600">
+            <![endif]-->
+            <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+              <tr>
+                <td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: 'Inter', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;">
+                  <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">New Form Submission</h1>
+                </td>
+              </tr>
+            </table>
+            <!--[if (gte mso 9)|(IE)]>
+            </td>
+            </tr>
+            </table>
+            <![endif]-->
+          </td>
+        </tr>
+    
+        <tr>
+          <td align="center" bgcolor="#e9ecef">
+            <!--[if (gte mso 9)|(IE)]>
+            <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+            <tr>
+            <td align="center" valign="top" width="600">
+            <![endif]-->
+            <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+    
+              <tr>
+                <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Inter', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
+                  <p style="margin: 0;">Hey! Your form at ${formName} has recieved a new submission! You can see it on formify or mark it as spam if it is spam.</p>
+                </td>
+              </tr>
+    
+              <tr>
+                <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Inter', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
+                  <p style="margin: 0;">If that doesn't work, copy and paste the following link in your browser:</p>
+                  <p style="margin: 0;"><a href="https://sendgrid.com" target="_blank">https://same-link-as-button.url/xxx-xxx-xxxx</a></p>
+                </td>
+              </tr>
+    
+              <tr>
+                <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Inter', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf">
+                  <p style="margin: 0;">Cheers,<br> Formify</p>
+                </td>
+              </tr>
+    
+            </table>
+            <!--[if (gte mso 9)|(IE)]>
+            </td>
+            </tr>
+            </table>
+            <![endif]-->
+          </td>
+        </tr>
+    
+        <tr>
+          <td align="center" bgcolor="#e9ecef" style="padding: 24px;">
+            <!--[if (gte mso 9)|(IE)]>
+            <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+            <tr>
+            <td align="center" valign="top" width="600">
+            <![endif]-->
+            <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+    
+              <tr>
+                <td align="center" bgcolor="#e9ecef" style="padding: 12px 24px; font-family: 'Inter', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
+                  <p style="margin: 0;">You received this email because you have turned on email notifications for this form. You can choose to opt-out of all notifications for this form by clicking here.</p>
+                </td>
+              </tr>
+    
+              <tr>
+                <td align="center" bgcolor="#e9ecef" style="padding: 12px 24px; font-family: 'Inter', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
+                  <p style="margin: 0;">To stop receiving these emails, you can <a href="https://sendgrid.com" target="_blank">unsubscribe</a> at any time.</p>
+                  <p style="margin: 0;">&copy; Formify, LLP </p>
+                </td>
+              </tr>
+    
+            </table>
+            <!--[if (gte mso 9)|(IE)]>
+            </td>
+            </tr>
+            </table>
+            <![endif]-->
+          </td>
+        </tr>
+    
+      </table>
+    
+    </body>
+    </html>
+    
+    `
+}
