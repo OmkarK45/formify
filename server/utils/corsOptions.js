bca97395a1658cb0ec33c105e6ca3768a2f585b3
Omkar Kulkarni <omkarak45@gmail.com>
2021-02-28 18:29:20 +0530

    Added cors

--- server/utils/corsOptions.js
+++ server/utils/corsOptions.js
@@ -1,6 +1,6 @@
 //  @TODO -[Security] Change this to frontend later
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://formify-beta.netlify.app"],
   optionsSuccessStatus: 200,
   credentials: true,
 }
