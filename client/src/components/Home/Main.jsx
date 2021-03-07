0431a02c583187bfd2b8436842093ede9a2b66ed
Omkar Kulkarni <omkarak45@gmail.com>
2021-03-07 16:29:29 +0530

    Brand new marketing page

--- client/src/components/Home/Main.jsx
+++ client/src/components/Home/Main.jsx
@@ -7,129 +7,138 @@ import {
   Heading,
   Icon,
   IconButton,
-  IconProps,
   Image,
   Stack,
   Text,
   useColorModeValue,
-  Link,
 } from "@chakra-ui/react"
+import { useContext } from "react"
 import { Link as RouterLink } from "react-router-dom"
+
+import { Features } from "."
+import userContext from "./../../context/userContext"
+
 export default function Main() {
+  const textBg = useColorModeValue("orange.300", "orange.600")
+  const { user } = useContext(userContext)
   return (
-    <Container maxW={"7xl"}>
-      <Stack
-        align={"center"}
-        spacing={{ base: 8, md: 10 }}
-        py={{ base: 20, md: 28 }}
-        direction={{ base: "column", md: "row" }}
-      >
-        <Stack flex={1} spacing={{ base: 5, md: 10 }}>
-          <Heading
-            lineHeight={1.1}
-            fontWeight={600}
-            fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}
-          >
-            <Text
-              as={"span"}
-              position={"relative"}
-              _after={{
-                content: "''",
-                width: "full",
-                height: "30%",
-                position: "absolute",
-                bottom: 1,
-                left: 0,
-                bg: "orange.200",
-                zIndex: -1,
-              }}
-            >
-              Handle form submissions better.
-            </Text>
-          </Heading>
-          <Text color={"gray.500"}>
-            Formify is a ready-to integrate form submission solution for static
-            sites. Reduce hassle and let us handle your form submissions! No
-            database configuration required.
-          </Text>
-          <Stack
-            spacing={{ base: 4, sm: 6 }}
-            direction={{ base: "column", sm: "row" }}
-          >
-            <Button
-              rounded={"full"}
-              as={RouterLink}
-              to="/auth"
-              size={"lg"}
-              fontWeight={"normal"}
-              px={6}
-              colorScheme={"orange"}
-              bg={"orange.500"}
-              _hover={{ bg: "orange.600" }}
-            >
-              Get started
-            </Button>
-            <Button
-              rounded={"full"}
-              size={"lg"}
-              fontWeight={"normal"}
-              px={6}
-              leftIcon={<PlayIcon h={4} w={4} color={"gray.300"} />}
-            >
-              How It Works
-            </Button>
-          </Stack>
-        </Stack>
-        <Flex
-          flex={1}
-          justify={"center"}
+    <>
+      <Container maxW={"7xl"}>
+        <Stack
           align={"center"}
-          position={"relative"}
-          w={"full"}
+          spacing={{ base: 8, md: 10 }}
+          py={{ base: 20, md: 28 }}
+          direction={{ base: "column", md: "row" }}
         >
-          <Blob
-            w={"150%"}
-            h={"150%"}
-            position={"absolute"}
-            top={"-20%"}
-            left={0}
-            zIndex={-1}
-            color={useColorModeValue("red.50", "red.400")}
-          />
-          <Box
+          <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+            <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}
+            >
+              <Text
+                as={"span"}
+                position={"relative"}
+                _after={{
+                  content: "''",
+                  width: "full",
+                  height: "30%",
+                  position: "absolute",
+                  bottom: 1,
+                  left: 0,
+                  bg: "yellow.500",
+                  zIndex: -1,
+                }}
+              >
+                Handle form submissions better.
+              </Text>
+            </Heading>
+            <Text color={"gray.500"}>
+              Formify is a ready-to integrate form submission solution for
+              static sites. Reduce hassle and let us handle your form
+              submissions! No database configuration required.
+            </Text>
+            <Stack
+              spacing={{ base: 4, sm: 6 }}
+              direction={{ base: "column", sm: "row" }}
+            >
+              <Button
+                rounded={"full"}
+                as={RouterLink}
+                to={user && user.isAuthenticated ? "/dashboard/forms" : "/auth"}
+                size={"lg"}
+                fontWeight={"normal"}
+                px={6}
+                colorScheme={"orange"}
+                bg={"orange.500"}
+                color="white"
+                _hover={{ bg: "orange.600" }}
+              >
+                Get started
+              </Button>
+              <Button
+                rounded={"full"}
+                size={"lg"}
+                fontWeight={"normal"}
+                px={6}
+                leftIcon={<PlayIcon h={4} w={4} color={"gray.300"} />}
+              >
+                How It Works
+              </Button>
+            </Stack>
+          </Stack>
+          <Flex
+            flex={1}
+            justify={"center"}
+            align={"center"}
             position={"relative"}
-            height={"300px"}
-            rounded={"2xl"}
-            boxShadow={"2xl"}
-            width={"full"}
-            overflow={"hidden"}
+            w={"full"}
           >
-            <IconButton
-              aria-label={"Play Button"}
-              variant={"ghost"}
-              _hover={{ bg: "transparent" }}
-              icon={<PlayIcon w={12} h={12} />}
-              size={"lg"}
-              color={"white"}
+            <Blob
+              w={"150%"}
+              h={"150%"}
               position={"absolute"}
-              left={"50%"}
-              top={"50%"}
-              transform={"translateX(-50%) translateY(-50%)"}
+              top={"-20%"}
+              left={0}
+              zIndex={-1}
+              color={useColorModeValue("red.50", "red.400")}
             />
-            <Image
-              alt={"Hero Image"}
-              fit={"cover"}
-              align={"center"}
-              w={"100%"}
-              h={"100%"}
-              src={
-                "https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80"
-              }
-            />
-          </Box>
-        </Flex>
-      </Stack>
-    </Container>
+            <Box
+              position={"relative"}
+              height={"300px"}
+              rounded={"2xl"}
+              boxShadow={"2xl"}
+              width={"full"}
+              overflow={"hidden"}
+            >
+              <IconButton
+                aria-label={"Play Button"}
+                variant={"ghost"}
+                _hover={{ bg: "transparent" }}
+                icon={<PlayIcon w={12} h={12} />}
+                size={"lg"}
+                color={"white"}
+                position={"absolute"}
+                left={"50%"}
+                top={"50%"}
+                transform={"translateX(-50%) translateY(-50%)"}
+              />
+              <Image
+                alt={"Hero Image"}
+                fit={"cover"}
+                align={"center"}
+                w={"100%"}
+                h={"100%"}
+                src={
+                  "https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80"
+                }
+              />
+            </Box>
+          </Flex>
+        </Stack>
+      </Container>
+      <Features />
+    </>
   )
 }
 
